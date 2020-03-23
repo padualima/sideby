50a329f707498bd59dfe79adf02ef55f08e75dec
Pádua Lima <padualima.dev@gmail.com>
2020-03-23 17:41:09 -0300

    test of controller added

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,8 +1,7 @@
 FactoryBot.define do
-
  factory :user do
    name         { FFaker::Lorem.word }
    email        { FFaker::Internet.email }
-   password     'secret123'
+   password     {'secret123'}
  end
 end
